9c74d261984e3afe2f7353e4fd2392303729a40d
Adam Long <adam.jacob.long@gmail.com>
2023-10-21 20:35:16 -0500

    CommonBody files created and developed. Everything runs off a template now.

---
+++ commonElements/commonBody.js
@@ -0,0 +1,31 @@
+function loadCommonBody()
+{
+    fetch("../commonElements/commonBody.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('commonBody').innerHTML = data;
+        });
+}
+
+function goHome()
+{
+    window.location.href = "../index.html";
+}
+
+function goAboutme()
+{
+    window.location.href = "../aboutMe/aboutme.html";
+}
+
+function goForBusiness()
+{
+    window.location.href = "../forBusiness/forBusiness.html";
+}
+
+function goForSocials()
+{
+    window.location.href = "../forSocials/forSocials.html";
+}
+
+//Runner
+loadCommonBody();
--- commonElements/commonHead.js
+++ commonElements/commonHead.js
@@ -7,7 +7,4 @@ function loadCommonHead()
         });
 }
 
-
-
-
 loadCommonHead();
