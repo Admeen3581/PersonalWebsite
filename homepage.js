7f768932840fa8c431cf4df0ebb4317617f3f9d8
Adam Long <adam.jacob.long@gmail.com>
2023-11-04 23:53:20 -0500

    AboutMe being developed. Added .txt -> HTML support.

--- homepage.js
+++ homepage.js
@@ -20,3 +20,24 @@ function goForSocials()
 {
     window.location.href = "forSocials/forSocials.html";
 }
+
+function loadTextFileToElement(path, id)
+{
+    let textFile = new XMLHttpRequest();
+    var textToWrite = "404 ERROR - FileNotFound";
+    textFile.onreadystatechange = function ()
+    {
+        if (textFile.readyState === 4 && textFile.status === 200)
+        {
+            textToWrite = textFile.responseText;
+            textToWrite = textToWrite.split("\n").join("<br>"); //Breaks the text into HTML breaks
+        }
+        try
+        {
+            document.getElementById(id).innerHTML = textToWrite;
+        }
+        catch (e) {/*above throws an Uncaught exception. Not sure why because it works?*/}
+    }
+    textFile.open("GET", path, true);
+    textFile.send();
+}
