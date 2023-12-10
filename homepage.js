2471ed8297253644131e295b8b3525cf413cc043
Adam Long <adam.jacob.long@gmail.com>
2023-12-09 23:09:36 -0600

    Navbar disappears with footer intersection.

--- homepage.js
+++ homepage.js
@@ -21,3 +21,63 @@ function loadTextFileToElement(path, id)
     textFile.open("GET", path, true);
     textFile.send();
 }
+
+function hideNavBarOnVisibleFooter()
+{
+    /*
+    const navbar = document.getElementById('navbar');
+    const footer = document.querySelector(".footerDetails");
+
+    if (navbar && footer)
+    {
+        window.addEventListener('scroll', relay(function ()
+        {
+            const rectNavbar = navbar.getBoundingClientRect();
+            const rectFoot = footer.getBoundingClientRect();
+
+            if (rectFoot && rectNavbar)
+            {
+                console.log("HELLOOOO");
+                if (rectNavbar.bottom >= rectFoot.top)
+                {
+                    navbar.classList.add("hidden");
+                } else {
+                    navbar.classList.remove("hidden");
+                }
+            }
+        }, 100));
+    }
+
+     */
+    document.addEventListener('DOMContentLoaded', relay(function () {
+        var navbar = document.getElementById('navbar');
+        var footer = document.getElementById('foot');
+
+        if (navbar && footer) {
+            var observer = new IntersectionObserver(function (entries) {
+                if (entries[0].isIntersecting) {
+                    navbar.classList.add('hidden');
+                } else {
+                    navbar.classList.remove('hidden');
+                }
+            });
+
+            observer.observe(footer);
+        }
+    }, 100));
+}
+
+function relay(func, delay)
+{
+    let timer;
+    return function()
+    {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function()
+        {
+            func.apply(context, args);
+        }, delay);
+    }
+}
